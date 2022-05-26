56c8d1f4a7a69d503ed66220b37f10aa273235ee
Jatabara <patrickluizjf@hotmail.com>
2022-05-26 19:21:16 -0300

    add

---
+++ relase/script.js
@@ -0,0 +1,15 @@
+
+
+const scanner = new Instascan.Scanner({
+    video: document.getElementById('webcam')
+})
+
+scanner.addListener('scan', content =>{
+    console.log(content)
+})
+
+Instascan.Camera.getCameras().then( cameras => {
+    if(cameras.legth > 0 ){
+        scanner.start(cameras[0])
+    }
+})
